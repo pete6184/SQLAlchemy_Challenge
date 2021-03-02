5d0cd6e4b0b603eced96187558aabaa8fb306fea
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-03-01 17:23:08 -0800

    saving before class

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 # Import packages
 import numpy as nu
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Sessions
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 
 # Database Setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -43,12 +40,18 @@ def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    
+    # Convert the query results to a dictionary using date as the key and prcp as the value.
+
+    # Return the JSON representation of your dictionary.
+
+
 
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
+    # Return a JSON list of stations from the dataset.
 
 
    
@@ -58,7 +61,10 @@ def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # Query the dates and temperature observations of the most active station for the last year of data.
 
+
+    # Return a JSON list of temperature observations (TOBS) for the previous year.
     
 
 @app.route("/api/v1.0/<start>")
@@ -66,7 +72,14 @@ def start_date():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # For the next two Routes
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 
+
+    # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+
+
+    # When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
     
 
 @app.route("/api/v1.0/<start>/<end>")
